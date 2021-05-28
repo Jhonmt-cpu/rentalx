239c01ce681f930846b4cc6226c0917a9c35ecc1
João Mateus <joaomateusabm@yahoo.com.br>
2021-05-28 12:47:11 -0300

    chore: implemented integrations test with supertest

---
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -0,0 +1,48 @@
+import { getRepository, IsNull, Repository } from "typeorm";
+
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+
+import { Rental } from "../entities/Rental";
+
+class RentalsRepository implements IRentalsRepository {
+  private repository: Repository<Rental>;
+
+  constructor() {
+    this.repository = getRepository(Rental);
+  }
+
+  async findOpenRentalByCar(car_id: string): Promise<Rental> {
+    const openByCar = await this.repository.findOne({
+      car_id,
+      end_date: IsNull(),
+    });
+
+    return openByCar;
+  }
+  async findOpenRentalByUser(user_id: string): Promise<Rental> {
+    const openByCar = await this.repository.findOne({
+      user_id,
+      end_date: IsNull(),
+    });
+
+    return openByCar;
+  }
+  async create({
+    car_id,
+    user_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = this.repository.create({
+      car_id,
+      expected_return_date,
+      user_id,
+    });
+
+    await this.repository.save(rental);
+
+    return rental;
+  }
+}
+
+export { RentalsRepository };
